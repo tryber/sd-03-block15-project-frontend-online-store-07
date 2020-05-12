4d856a6c3058a6a70c1ac40fff764261f85888c6
Hfreitas <hfreitas86@gmail.com>
2020-05-12 16:07:01 -0300

    criação do componente card para listagem de produtos

---
+++ src/components/CardProduto.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class CardProduto extends Component {
+  render() {
+    const { product } = this.props;
+    const { title, imagePath, price, onAddToCart } = product;
+    return (
+      <div>
+        <h4 data-testid="product">{title}</h4>
+        <img data-testid="product" src={imagePath} alt={title} />
+        <h5 data-testid="product">{price}</h5>
+        <button
+          type="button"
+          onClick={onAddToCart}
+          data-testid="product-add-to-cart"
+        >
+          Adicionar ao Carrinho
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CardProduto;
