c8d82af2971c5827d510d94c128e62902e8149d9
Hfreitas <hfreitas86@gmail.com>
2020-05-12 19:31:13 -0300

    acrescido id de teste correto ao componente CarLink

--- src/components/CarLink.jsx
+++ src/components/CarLink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export class CarLink extends Component {
   render() {
     return (
-      <Link to="/cart">
+      <Link data-testid ="shopping-cart-button" to="/cart">
         <i className="fas fa-shopping-cart" />
       </Link>
     );
