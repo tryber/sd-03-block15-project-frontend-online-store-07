169f2b3662f3f3c83ce6a3dcf83d6fbe1ccc754d
potew <potew0@gmail.com>
2020-05-12 19:31:54 -0300

    Update BarraEsquerda.jsx

--- src/components/BarraEsquerda.jsx
+++ src/components/BarraEsquerda.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import * as api from '../services/api'
+import React, { Component } from 'react';
+import * as api from '../services/api';
 
 class BarraEsquerda extends Component {
   constructor(props) {
     super(props);
-    this.state = {categorias: []};
+    this.state = { categorias: [] };
   }
 
   componentDidMount() {
-    api.getCategories().then((data) => this.setState({categorias: data}))
+    api.getCategories().then((data) => this.setState({ categorias: data }));
   }
 
   render() {
     const { categorias } = this.state;
     return (
       <div className="categoriesMenu">
-        {categorias.map(cat => <li key={cat.id}>{cat.name}</li>)}
+        {categorias.map((cat) => <li key={cat.id}>{cat.name}</li>)}
       </div>
     );
   }
