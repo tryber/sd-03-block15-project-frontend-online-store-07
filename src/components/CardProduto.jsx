3815864fe69e741a825fef9b06959d6fdeb77f46
Hfreitas <hfreitas86@gmail.com>
2020-05-13 16:45:13 -0300

    acrescidas informações sobre frete e quantidade disponivel por produto, criada lógica para acrescentar item ao localStorage

--- src/components/CardProduto.jsx
+++ src/components/CardProduto.jsx
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
 
 export class CardProduto extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart() {
+    console.log(localStorage.products);
+    const { product } = this.props;
+    const cartItems = parseInt(localStorage.getItem('cartItems'), 10) || 0;
+    if (!localStorage.products || localStorage.getItem('products') === 'null') {
+      localStorage.setItem('cartItems', cartItems + 1);
+      localStorage.setItem('products', JSON.stringify([product]));
+    }
+    const products = JSON.parse(localStorage.getItem('products'));
+    if (localStorage.products.includes(product.id)) {
+      const index = products.findIndex((item) => item.id === product.id);
+      products[index].quantity += 1;
+      localStorage.setItem('cartItems', cartItems + 1);
+      localStorage.setItem('products', JSON.stringify(products));
+    }
+    localStorage.setItem('cartItems', cartItems + 1);
+    localStorage.setItem('products', JSON.stringify([...products, product]));
+  }
+
   render() {
     const { product } = this.props;
-    const { id, title, thumbnail, price, onAddToCart } = product;
+    const {
+      id,
+      title,
+      thumbnail,
+      price,
+      available_quantity,
+      shipping: { free_shipping },
+    } = product;
     return (
       <div data-testid="product">
         <h4>{title}</h4>
         <h5>{id}</h5>
         <img src={thumbnail} alt={title} />
-        <h5>{price}</h5>
+        <h5>
+          Preço: R$
+          {price.toFixed(2)}
+        </h5>
+        <h5>
+          Quantidade disponível:
+          {available_quantity}
+        </h5>
+        {free_shipping === true ? <h6>FRETE GRÁTIS</h6> : <h6>FRETE PAGO</h6>}
         <button
           type="button"
-          onClick={onAddToCart}
+          onClick={this.addToCart}
           data-testid="product-add-to-cart"
         >
           Adicionar ao Carrinho
