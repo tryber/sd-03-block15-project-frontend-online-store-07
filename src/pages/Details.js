a95216697691bee3b4a878010aa9be40137c25d6
potew <potew0@gmail.com>
2020-05-13 21:10:06 -0300

    CC

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 export class Details extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      quantidade: 0,
-    };
+    this.state = { quantidade: 0 };
     this.atualizaQuantidade = this.atualizaQuantidade.bind(this);
     this.alteraQuantidade = this.alteraQuantidade.bind(this);
   }
@@ -17,30 +15,32 @@ export class Details extends Component {
 
   alteraQuantidade(soma, max) {
     this.setState((currentState) => {
-      const novaQuantidade = (soma ? currentState.quantidade + 1 : currentState.quantidade - 1)
-      if (novaQuantidade >= 0 && novaQuantidade < max)
-      return ({quantidade: novaQuantidade})
+      const novaQuantidade = (soma ? currentState.quantidade + 1 : currentState.quantidade - 1);
+      if (novaQuantidade >= 0 && novaQuantidade < max) {
+        return ({ quantidade: novaQuantidade });
+      }
+      return ({ quantidade: 0 })
     });
   }
 
   seletorQuantidade() {
-    const {available_quantity} = this.props.apiResults;
-    return(
+    const estoque = this.props.apiResults.available_quantity;
+    return( 
       <div>
-        <label for='quantidade'>Quantidade</label>: 
+        <label htmlFor="quantidade">Quantidade</label>:
         <input
           name="quantidade"
           type="number"
           min="0"
-          max={available_quantity}
+          max={estoque}
           value={this.state.quantidade}
-          onChange={this.atualizaQuantidade}>
-        </input>
-        <button onClick={() => this.alteraQuantidade(false, available_quantity)}>-</button>
-        <button onClick={() => this.alteraQuantidade(true, available_quantity)}>+</button>
+          onChange={this.atualizaQuantidade}
+        />
+        <button onClick={() => this.alteraQuantidade(false, estoque)}>-</button>
+        <button onClick={() => this.alteraQuantidade(true, estoque)}>+</button>
         <button>Adicionar ao carrinho</button>
       </div>
-    )
+    );
   }
 
   render() {
@@ -48,17 +48,17 @@ export class Details extends Component {
     return (
       <div className="detalheProduto">
         <div className="cabecalhoProduto">
-          <Link to='/'>Voltar</Link>
-          <h3 data-testid='product-detail-name'>{title} - R$ {price}</h3>
+          <Link to="/">Voltar</Link>
+          <h3 data-testid="product-detail-name">{title} - R$ {price}</h3>
         </div>
         <div className="imagemDetalhe">
-          <img src={thumbnail} alt=""></img>
+          <img src={thumbnail} alt="" />
         </div>
         <div className="dadosProduto">
           <h3>Especificações técnicas</h3>
           {attributes.map((att) => <li>{att.name}: {att.value_name}</li>)}
         </div>
-          {this.seletorQuantidade()}
+        {this.seletorQuantidade()}
       </div>
     );
   }
