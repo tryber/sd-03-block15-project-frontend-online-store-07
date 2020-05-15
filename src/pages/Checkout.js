04ad6fb071f7dae1eab5cba3dc1f86748c033fc0
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-05-15 16:59:36 -0300

    Cc

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,20 +4,7 @@ import { GridCheckout } from '../components/GridCheckout';
 import CheckoutForm from '../components/CheckoutForm';
 
 class Checkout extends Component {
-  constructor() {
-    super();
-    this.state = {
-      falseProducts: [{
-        id: 'MLB1370656442',
-        title: 'Motorola G7 Play 32 Gb Dourado 2 Gb Ram',
-        price: 849,
-        thumbnail: 'http://mlb-s1-p.mlstatic.com/964021-MLA31350197875_072019-I.jpg',
-      }],
-    };
-  }
-
   render() {
-    const { falseProducts } = this.state;
     const { products } = this.props.location.state;
     console.log(products);
     return (
