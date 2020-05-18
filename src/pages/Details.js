f415c714ead0fabb110bf2d1851fbb5b22f6b6c1
Hfreitas <hfreitas86@gmail.com>
2020-05-18 19:20:09 -0300

    commitando para avaliação

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -188,12 +188,9 @@ export class Details extends Component {
   }
 
   botaoAdicionarAoCarrinho() {
-      const {
-        location: {
-          state: { product },
-        },
-      } = this.props;
-      const { id, price, title, thumbnail, availableQuantity} = product;
+    const {
+      location: { state: { product } } } = this.props;
+    const { id, price, title, thumbnail, availableQuantity } = product;
     return (
       <button
         data-testid="product-detail-add-to-cart"
@@ -207,11 +204,7 @@ export class Details extends Component {
 
   render() {
     const { quantidadeCarrinho } = this.state;
-    const {
-      location: {
-        state: { product },
-      },
-    } = this.props;
+    const { location: { state: { product } } } = this.props;
     const { id, price, title, thumbnail, attributes } = product;
     return (
       <div className="telaDetalhes">
